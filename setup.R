6bfd797a182d129efb7f3208e3e1933f1d7ea3f0
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2025-08-26 17:37:48 +0200

    Add needed packages

---
+++ setup.R
@@ -0,0 +1,13 @@
+
+# tcltk - to display the CRAN windows when not active. Relevant for Emacs
+# DescTools - for Winsorize function
+pkgs <- c("skimr", "codebook", "tcltk", "viridisLite",
+          "DescTools", "gt", "ggplot2", "here", "haven",
+          "rlang", "highcharter", "collapse", "data.table")
+
+invisible(lapply(pkgs, function(pkg) {
+    if (!requireNamespace(pkg, quietly = TRUE)) install.packages(pkg)
+    library(pkg, character.only = TRUE)
+}))
+
+Sys.setlocale("LC_ALL", "nb-NO.UTF-8")
