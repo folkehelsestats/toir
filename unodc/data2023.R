c907cd04bca55d56377000cc249301a8bec3ab50
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2025-08-27 15:39:32 +0200

    Calculate prevalence

--- unodc/data2023.R
+++ unodc/data2023.R
@@ -1,15 +1,15 @@
-
+## Rusundersøkelse 2023
+## --------------------
 source(file.path(here::here(), "setup.R"))
+## readClipboard()
+odrive <- "O:\\Prosjekt\\Rusdata"
+rusdrive <- "Rusundersøkelsen\\Rusus historiske data\\ORG\\alkohol_rusundersokelsen"
+filpath <- file.path(odrive, rusdrive)
+filer <- grep("dta$", list.files(filpath), value = TRUE)
 
-## Data 2024
-## --------------
-mainpath <- "O:\\Prosjekt\\Rusdata"
-## DT <- haven::read_dta(file.path("Rusundersøkelsen", "Rusus 2024", "nytt forsøk februar 25 rus24.dta"))
-## saveRDS(DT, file.path("Rusundersøkelsen", "Rusus 2024","rus2024.rds"))
-DT <- readRDS(file.path(mainpath, "Rusundersøkelsen", "Rusus 2024","rus2024.rds"))
-dt <- as.data.table(DT)
+dt <- haven::read_dta(file.path(filpath, grep("2023", filer, value = T)))
+setDT(dt)
 
-## Andre narkotske stoffer
 grep("Ans", names(dt), value = T)
 
 ## Denominator
@@ -48,3 +48,18 @@ dt[Ans3_7 == 1, lyp_lsd := 1]
 ## -------------------------
 
 dt[Can10 == 1, lmp_cannabis := 1]
+
+
+## no - nominator
+## de - denominator
+cal_prev <- function(dt, no, de){
+  dt[, sum(no, na.rm = T)/sum(de, na.rm = T)*100, env = list(no = no, de = de)]
+}
+
+cal_prev(dt, "ltp_cannabis", "canpop")
+
+## dummy denominator
+dt[!is.na(Can1), canpop2 := 1]
+dt[, canpop3 := 0][!(Can1 %in% 8:9), canpop3 := 1] #alle
+
+cal_prev(dt, "ltp_cannabis", "canpop2")
