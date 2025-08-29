f734e288f7ee325fdd8d323518f374f3bc432178
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2025-08-29 14:13:10 +0200

    Add any type drugs

--- unodc/data2024.R
+++ unodc/data2024.R
@@ -14,6 +14,11 @@ dt <- as.data.table(DT)
 ## Columnames for andre narkotiske stoffer
 grep("Ans", names(dt), value = T)
 
+## Age groups
+## -------------
+dt <- group_age_standard(dt, var = "Alder", type = "unodc",
+                         new_var = "agecat")
+
 ## Denominator
 ## ------------
 dt[, canpop := fcase(Can1 %in% 1:2, 1,
@@ -22,20 +27,32 @@ dt[, canpop := fcase(Can1 %in% 1:2, 1,
 dt[, narkpop := fcase(Ans1 %in% 1:2, 1,
                       default = 0)]
 
+dt[canpop == 1 | narkpop == 1, anypop := 1][
+  is.na(anypop), anypop := 0]
+
+## ---------------------
 ## Lifetime  prevalence
 ## ---------------------
 ## Bør sjekke tekst fra Ans2sps
 ## dt[, .N, keyby = Ans2sps][!grep("9999", Ans2sps)]
 
-dt[Can1 == 1, ltp_cannabis := 1]
-dt[Ans1 == 1, ltp_any := 1]
-dt[Ans2_a == 1, ltp_cocaine := 1]
-dt[Ans2_b == 1, ltp_mdma := 1]
-dt[Ans2_c == 1, ltp_amphetamines := 1]
+dt[Can1 == 1, ltp_cannabis := 1] #Cannabis-type drugs
+dt[Ans1 == 1, ltp_other := 1]
+
+## Any drug
+dt[, ltp_any := fcase(Ans1 == 1, 1,
+                      ltp_cannabis == 1, 1,
+                      default = 0)]
+
+dt[Ans2_a == 1, ltp_cocaine := 1] #Cocaine-type drugs
+dt[Ans2_b == 1, ltp_mdma := 1] #"Ecstasy" type substances
+dt[Ans2_c == 1, ltp_amphetamines := 1] #Amphetamine-type stimulants
 dt[Ans2_d == 1, ltp_relevin := 1]
-dt[Ans2_e == 1, ltp_heroin := 1]
-dt[Ans2_f == 1, ltp_ghb := 1]
-dt[Ans2_g == 1, ltp_lsd := 1]
+dt[Ans2_e == 1, ltp_heroin := 1] #Heroin
+dt[Ans2_f == 1, ltp_ghb := 1] #Other sedatives and tranquillizers
+dt[Ans2_g == 1, ltp_lsd := 1] #LSD
+
+
 
 ## Last year prevalence
 ## --------------------
@@ -54,11 +71,6 @@ dt[Ans3_7 == 1, lyp_lsd := 1]
 
 dt[Can10 == 1, lmp_cannabis := 1]
 
-## Age groups
-## -------------
-
-dt <- group_age_standard(dt, var = "Alder", type = "unodc",
-                         new_var = "agecat")
 
 cal_prev(dt, "ltp_cannabis", "canpop")
-
+cal_prev(dt, "ltp_heroin", "narkpop")
