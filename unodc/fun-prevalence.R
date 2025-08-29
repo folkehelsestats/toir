505022eb09946350c79162fea7b348dd64c7a7e7
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2025-08-29 16:16:48 +0200

    Add wrapping to calc_percentage

--- unodc/fun-prevalence.R
+++ unodc/fun-prevalence.R
@@ -1,4 +1,58 @@
 
+no = "ltp_cannabis"
+de = "canpop"
+weight_var = "VEKT"
+group_vars = "Kjonn"
+
+## no - nominator
+## de - denominator
+## weight_var NULL if unweighted
+get_prev <- function(dt, no, de, weight_var ="VEKT"){
+
+  tot <- calc_percentage(dt=dt,
+                         outcome_var = no,
+                         weight_var = weight_var,
+                         denominator_var = de,
+                         na_treatment = "as_zero",
+                         round_digits = 1,
+                         include_diagnostics = FALSE)
+
+  kjonn <- calc_percentage(dt=dt,
+                           outcome_var = no,
+                           group_vars = "Kjonn",
+                           weight_var = weight_var,
+                           denominator_var = de,
+                           na_treatment = "as_zero",
+                           round_digits = 1,
+                           include_diagnostics = FALSE)
+
+  alder <- calc_percentage(dt=dt,
+                           outcome_var = no,
+                           group_vars = "agecat",
+                           weight_var = weight_var,
+                           denominator_var = de,
+                           na_treatment = "as_zero",
+                           round_digits = 1,
+                           include_diagnostics = FALSE)
+
+  data.table::setorder(alder, "agecat")
+
+  begge <- calc_percentage(dt=dt,
+                           outcome_var = no,
+                           group_vars = c("Kjonn", "agecat"),
+                           weight_var = weight_var,
+                           denominator_var = de,
+                           na_treatment = "as_zero",
+                           round_digits = 1,
+                           include_diagnostics = FALSE)
+
+  data.table::setorder(begge, "Kjonn", "agecat")
+
+  list(total = tot, kjonn = kjonn, alder = alder, begge = begge)
+}
+
+## Naive function
+## -------------
 ## no - nominator
 ## de - denominator
 cal_prev <- function(dt, no, de){
