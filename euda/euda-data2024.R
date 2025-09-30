5c38ecab0d45812b73e37d47e2a614d6d81f1a2a
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2025-10-01 00:15:28 +0200

    Match output as form style

--- euda/euda-data2024.R
+++ euda/euda-data2024.R
@@ -3,7 +3,9 @@
 
 source(file.path(here::here(), "setup.R"))
 source("https://raw.githubusercontent.com/folkehelsestats/toa/refs/heads/main/rusund/functions/fun-age.R")
+source("https://raw.githubusercontent.com/folkehelsestats/toa/refs/heads/main/rusund/functions/fun-percent-weighted.R")
 source(file.path(here::here(), "unodc","fun-weighted-unweighted-ci.R"))
+source(file.path(here::here(), "unodc","fun-weighted-unweighted-ci-flexible.R"))
 source(file.path(here::here(), "unodc","fun-prevalence-ci.R"))
 source(file.path(here::here(), "unodc","fun-pct-change.R"))
 source(file.path(here::here(), "euda","fun-form-style.R"))
@@ -82,9 +84,6 @@ dt[Can1_ny == 1, ltp_cannabis := 1] #Cannabis-type drugs
 dt[Ans1 == 1, ltp_other := 1]
 
 ## Any drug
-dt[, ltp_any := fcase(Ans1 == 1, 1,
-                      ltp_cannabis == 1, 1,
-                      default = 0)]
 
 dt[Ans2_a == 1, ltp_cocaine := 1] #Cocaine-type drugs
 dt[Ans2_b == 1, ltp_mdma := 1] #"Ecstasy" type substances
@@ -136,21 +135,22 @@ get_prev_ci(dty, "ltp_tobacco", "tobpop")
 
 # Broad age groups
 # With 95%CI - All Adults
-show_form(dt, "ltp_any", "anypop") #Anyrug
-show_form(dt, "ltp_cannabis", "canpop") #Cannabis-type drugs
-show_form(dt, "ltp_heroin", "narkpop") #Heroin
-show_form(dt, "ltp_cocaine", "narkpop") #Cocaine-type drugs
-show_form(dt, "ltp_amphetamines", "narkpop") #Amphetamine-type stimulants
-show_form(dt, "ltp_mdma", "narkpop") #"Ecstasy" type substances
-show_form(dt, "ltp_ghb", "narkpop") #Other sedatives and tranquilizers
-show_form(dt, "ltp_lsd", "narkpop") #LSD
-show_form(dt, "ltp_steroid", "doppop")
-show_form(dt, "ltp_alcohol", "alkopop")
-show_form(dt, "ltp_tobacco", "tobpop")
+broad_form(dt, "ltp_any", "anypop") #Anyrug
+broad_form(dt, "ltp_cannabis", "canpop") #Cannabis-type drugs
+broad_form(dt, "ltp_heroin", "narkpop") #Heroin
+broad_form(dt, "ltp_cocaine", "narkpop") #Cocaine-type drugs
+broad_form(dt, "ltp_amphetamines", "narkpop") #Amphetamine-type stimulants
+broad_form(dt, "ltp_mdma", "narkpop") #"Ecstasy" type substances
+broad_form(dt, "ltp_ghb", "narkpop") #Other sedatives and tranquilizers
+broad_form(dt, "ltp_lsd", "narkpop") #LSD
+broad_form(dt, "ltp_steroid", "doppop")
+broad_form(dt, "ltp_alcohol", "alkopop")
+broad_form(dt, "ltp_tobacco", "tobpop")
 
 ## Last year prevalence
 ## --------------------
 
+# Recode
 dt[Can6 == 1, lyp_cannabis := 1]
 dt[Ans3_1 == 1, lyp_cocaine := 1]
 dt[Ans3_2 == 1, lyp_mdma := 1]
@@ -176,18 +176,150 @@ dt[Drukket1 == 1, lyp_alcohol := 1]
 #gen ltp_tobacco=(Tob1==1 | Tob3==1 | Tob3==2) if Tob1<=2
 # Hva med Tob14?
 
-get_prev_ci(dt, "lyp_any", "anypop") #Anydrugs
-get_prev_ci(dt, "lyp_cannabis", "canpop") #Cannabis-type drugs
-get_prev_ci(dt, "lyp_heroin", "narkpop", diagnostic = FALSE) #Heroin
-get_prev_ci(dt, "lyp_cocaine", "narkpop") #Cocaine-type drugs
-get_prev_ci(dt, "lyp_amphetamines", "narkpop") #Amphetamine-type stimulants
-get_prev_ci(dt, "lyp_mdma", "narkpop") #"Ecstasy" type substances
-get_prev_ci(dt, "lyp_ghb", "narkpop") #Other sedatives and tranquilizers
-get_prev_ci(dt, "lyp_lsd", "narkpop") #LSD
+general_form(dt, "lyp_any", "anypop") #Anydrugs
+general_form(dt, "lyp_cannabis", "canpop") #Cannabis-type drugs
+general_form(dt, "lyp_heroin", "narkpop") #Heroin
+general_form(dt, "lyp_cocaine", "narkpop") #Cocaine-type drugs
+general_form(dt, "lyp_amphetamines", "narkpop") #Amphetamine-type stimulants
+general_form(dt, "lyp_mdma", "narkpop") #"Ecstasy" type substances
+general_form(dt, "lyp_ghb", "narkpop") #Other sedatives and tranquilizers
+general_form(dt, "lyp_lsd", "narkpop") #LSD
+general_form(dt, "lyp_steroid", "doppop")
+general_form(dt, "lyp_alcohol", "alkopop")
+
+
+# Young Adults (15-34)
+dty <- dt[Alder < 35]
+
+general_form(dty, "lyp_any", "anypop") #Anydrugs
+general_form(dty, "lyp_cannabis", "canpop") #Cannabis-type drugs
+general_form(dty, "lyp_heroin", "narkpop") #Heroin
+general_form(dty, "lyp_cocaine", "narkpop") #Cocaine-type drugs
+general_form(dty, "lyp_amphetamines", "narkpop") #Amphetamine-type stimulants
+general_form(dty, "lyp_mdma", "narkpop") #"Ecstasy" type substances
+general_form(dty, "lyp_ghb", "narkpop") #Other sedatives and tranquilizers
+general_form(dty, "lyp_lsd", "narkpop") #LSD
+general_form(dty, "lyp_steroid", "doppop")
+general_form(dty, "lyp_alcohol", "alkopop")
+
+# Broad age groups
+broad_form(dt, "lyp_any", "anypop") #Anydrugs
+broad_form(dt, "lyp_cannabis", "canpop") #Cannabis-type drugs
+broad_form(dt, "lyp_heroin", "narkpop") #Heroin
+broad_form(dt, "lyp_cocaine", "narkpop") #Cocaine-type drugs
+broad_form(dt, "lyp_amphetamines", "narkpop") #Amphetamine-type stimulants
+broad_form(dt, "lyp_mdma", "narkpop") #"Ecstasy" type substances
+broad_form(dt, "lyp_ghb", "narkpop") #Other sedatives and tranquilizers
+broad_form(dt, "lyp_lsd", "narkpop") #LSD
+broad_form(dt, "lyp_steroid", "doppop")
+broad_form(dt, "lyp_alcohol", "alkopop")
+
 
 ## Last month prevalence
 ## -------------------------
 
 dt[Can10 == 1, lmp_cannabis := 1]
+dt[Drukket3 == 1, lmp_alcohol := 1]
 
-get_prev_ci(dt, "lmp_cannabis", "canpop")
+# All adults
+general_form(dt, "lmp_cannabis", "canpop")
+general_form(dt, "lmp_alcohol", "alkopop")
+
+# Young adults (15-34)
+dty <- dt[Alder < 35]
+general_form(dty, "lmp_cannabis", "canpop")
+general_form(dty, "lmp_alcohol", "alkopop")
+
+# Broad age groups
+broad_form(dt, "lmp_cannabis", "canpop")
+broad_form(dt, "lmp_alcohol", "alkopop")
+
+## Frequency with N and %
+## -----------------------
+
+get_prev_ci(dt, "lmp_cannabis", "canpop", diagnostic = TRUE) #Cannabis-type drugs
+
+freqVal <- c("20 dager eller mer" = 1,
+             "10-19 dager" = 2,
+             "4-9 dager" = 3,
+             "1-3 dager" = 4)
+
+cannabis_form <- function(dt, group_vars = NULL, dim = NULL){
+
+  # dim - extra dimension to show in final tabel
+  canFreqTot <- calc_percentage_flexible(dt = dt,
+                                         outcome_var = "Can11",
+                                         outcome_type = "categorical",
+                                         group_vars = group_vars,
+                                         weight_var = "VEKT",
+                                         denominator_var = "canpop")
+  canFreq <- calc_percentage_flexible(dt = dt,
+                                      outcome_var = "Can11",
+                                      outcome_type = "categorical",
+                                      group_vars = group_vars,
+                                      weight_var = "VEKT",
+                                      denominator_var = "canpop")
+
+  canKB <- data.table::data.table(v1 = as.integer(freqVal),
+                                  v2 = names(freqVal))
+
+  canFreq[kjonnKB, on = c(Kjonn = "v1"), gender := i.v2]
+  canFreq[canKB, on = c(outcome_level = "v1"), canfreq := i.v2]
+  canFreqTot[canKB, on = c(outcome_level = "v1"), canfreq := i.v2][, gender := "Total"]
+
+  canCols <- c("gender","canfreq", "percentage")
+
+  if (!is.null(dim))
+    canCols <- c(canCols, dim)
+
+    list(
+      canFreqTot[, ..canCols],
+      canFreq[Kjonn == 0, ..canCols],
+      canFreq[Kjonn == 1, ..canCols]
+    )
+}
+
+cannabis_form(dt, "Kjonn")
+
+# Young adults (15-34)
+dty <- dt[Alder < 35]
+get_prev_ci(dty, "lmp_cannabis", "canpop", diagnostic = TRUE) #Cannabis-type drugs
+cannabis_form(dty, "Kjonn")
+
+# Broad age groups (number of cases)
+canBroadTot <- calc_percentage_flexible(dt, "lmp_cannabis", group_vars = c("agecat"), weight_var = "VEKT", denominator_var = "canpop")
+canBroadTot[order(agecat)][, .(agecat, n_level)]
+
+canBroad <- calc_percentage_flexible(dt, "lmp_cannabis", group_vars = c("Kjonn", "agecat"), weight_var = "VEKT", denominator_var = "canpop")
+canBroad[kjonnKB, on = c(Kjonn = "v1"), gender := i.v2][order(agecat, Kjonn)][, .(gender, agecat, n_level)]
+
+# Frequency percentages
+cannabis_form(dt, c("Kjonn", "agecat"), dim = "agecat")
+
+
+canFreqTot <- calc_percentage_flexible(dt = dt,
+                                       outcome_var = "Can11",
+                                       outcome_type = "categorical",
+                                       group_vars = c("agecat"),
+                                       weight_var = "VEKT",
+                                       denominator_var = "canpop")
+
+canFreqTot[canKB, on = c(outcome_level = "v1"), canfreq := i.v2]
+canFreqTot[, gender := "Total"]
+## canFreqTot <- canFreqTot[order(agecat, outcome_level), .(agecat, canfreq, percentage)]
+
+canFreqGender <- calc_percentage_flexible(dt = dt,
+                                       outcome_var = "Can11",
+                                       outcome_type = "categorical",
+                                       group_vars = c("Kjonn", "agecat"),
+                                       weight_var = "VEKT",
+                                       denominator_var = "canpop")
+
+canKB <- data.table::data.table(v1 = as.integer(freqVal),
+                                v2 = names(freqVal))
+
+canFreqGender[kjonnKB, on = c(Kjonn = "v1"), gender := i.v2]
+canFreqGender[canKB, on = c(outcome_level = "v1"), canfreq := i.v2]
+## canFreqGender <- canFreqGender[order(Kjonn, agecat, outcome_level), .(gender, agecat, canfreq, percentage)]
+
+broad_form_cat(canFreqTot, canFreqGender)
--- euda/fun-form-style.R
+++ euda/fun-form-style.R
@@ -1,10 +1,26 @@
+## Gender and total prevalence
+## ---------------------------
+
+general_form <- function(dt, no, de, ...){
+
+  d <- get_prev_ci(dt = dt, no = no, de = de, ...)
+
+  d$total[, gender := "Total"]
+  dx <- rbindlist(list(d$total, d$kjonn), use.names = TRUE, fill = TRUE)
+  dx[, Kjonn := NULL]
+  dx[, gender := factor(gender, levels = c("Male", "Female", "Total"))]
+  data.table::setcolorder(dx, "gender", before = "percentage")
+  dx <- dx[order(gender)]
+  return(dx)
+}
+
 ## Form style - 2.3 Broad age group
 ## --------------------------------
 # make life easier to match the form style
 
-show_form <- function(dt, no, de){
+broad_form <- function(dt, no, de, ...){
 
-  d <- get_prev_ci(dt = dt, no = no, de = de)
+  d <- get_prev_ci(dt = dt, no = no, de = de, ...)
 
   d$alder[, gender := "Total"]
   dx <- rbindlist(list(d$begge, d$alder), use.names = TRUE, fill = TRUE)
@@ -21,5 +37,25 @@ show_form <- function(dt, no, de){
   }
 
   names(dd) <- paste0("Agegp: ", age)
+  return(dd)
+}
+
+# tot - Dataset for total
+# cat - dataset for gender categories
+broad_form_cat <- function(tot, cat){
+
+  dx <- rbindlist(list(tot, cat), use.names = TRUE, fill = TRUE)
+  dx <- dx[order(agecat, Kjonn, outcome_level), .(agecat, gender, percentage, canfreq)]
+
+  can <- unique(dx$canfreq)
+
+  dd <- vector(mode = "list", length = length(can))
+
+  for (i in seq_len(length(can))){
+    x <- can[i]
+    dd[[i]] <- dx[canfreq == x]
+  }
+
+  names(dd) <- paste0("Cannabis freq: ", can)
   return(dd)
 }
