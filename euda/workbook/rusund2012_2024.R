2a26dea3d20b3a9f94a4ebfd1e631ae476ffcd86
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2025-10-21 13:51:15 +0200

    Clean up attributes

--- euda/workbook/rusund2012_2024.R
+++ euda/workbook/rusund2012_2024.R
@@ -1,18 +1,25 @@
-if (!requireNamespace("here", quietly = TRUE)) install.packages("here")
-library("here")
+## if (!requireNamespace("here", quietly = TRUE)) install.packages("here")
+## library("here")
 
 ## ## ## Line Diagram
 ## source(file.path(here::here(), "euda/workbook/fun-line-plot.R"))
+## source(file.path(here::here(), "euda/workbook/fun-prosent.R"))
 
 dataPath <- "O:\\Prosjekt\\Rusdata"
 
 ## Data 2012 - 2024
 ddt <- readRDS(file.path(dataPath, "Rusundersøkelsen", "Rusus historiske data", "data_2012_2024.rds"))
+data.table::setDT(ddt)
 
+# clean labelled attributes coz it makes troubles
+ddtc <- ddt[, lapply(.SD, \(col) {
+  attributes(col) <- NULL
+  col
+})]
 
-hasj <- calc_pro(ddt, "can6", "can7_a", "hasj")
-marihuana <- calc_pro(ddt, "can6", "can7_b", "marihuana")
-canoil <- calc_pro(ddt, "can6", "can7_c", "canoil")
+hasj <- calc_pro(ddtc, "can6", "can7_a", "hasj")
+marihuana <- calc_pro(ddtc, "can6", "can7_b", "marihuana")
+canoil <- calc_pro(ddtc, "can6", "can7_c", "canoil")
 
 dcan <- data.table::rbindlist(list(hasj, marihuana, canoil), use.names = TRUE)
 dcan[can == "hasj", canlab := "Resin"]
@@ -21,13 +28,30 @@ dcan[can == "canoil", canlab := "Cannabis oil"]
 
 chc4 <- c("#5F9EA0", "#E1B378", "#BB7BDA", "#7C145C")
 
-can12types <- make_line_plot(data = dcan,
-                             x = "year",
-                             y = "pro",
-                             color = "can",
-                             title = "Type of cannabis used last 12 months all adults (16-64), 2012-2024",
-                             color_values = chc4,
-                             label_col = "canlab",
-                             y_breaks = seq(0,80,10),
-                             y_lab = "Percentage",
-                             hvjust = c(-0.3, 0))
+annotdf <- dcan[year == 2021]
+annotdf[, loc := fcase(can == "hasj", pro + 2 ,
+                      can == "marihuana", pro - 2,
+                      can == "canoil", pro - 2)]
+
+library(ggplot2)
+## Linje diagram
+can12types <- ggplot() +
+  geom_line(data = dcan[can != "total"], aes(x = year, y = pro, color = can), linewidth = 1.5) +
+  geom_line(data = dcan[can == "total"], aes(x = year, y = pro, color = can), linewidth = 3) +
+  scale_color_manual(values = chc4) +
+  ## geom_text(data = dcan[year == 2024], aes(x = year, y = pro, label = canlab), hjust = -0.1) + #hjust -0.1 nudges label slightly to the right
+  scale_x_continuous(
+    limits = c(2011, 2024),
+    breaks = seq(2011, 2024, 1)
+    ## expand = expansion(mult = c(0.03, 0.25)) # Add 10% space to the right
+  ) +
+  scale_y_continuous(breaks = seq(0, max(dcan$pro) + 10, 10)) +
+  theme_classic() +
+  labs(title = "Number of cannabis seizures (resin and herbal/plants), 2011-2024",
+       y = "", x = "") +
+  theme(
+    panel.grid.major.y = element_line(color = "grey80", linewidth = 0.5, linetype = "dashed")
+  ) +
+  guides(color = "none") +
+  ## annotate("text", x = rep(2021, 3), y = c(4, 53, 75), label = c("canoil", "mari", "hasj"))
+  geom_text(data = annotdf, aes(x = year, y = loc, label = canlab))
