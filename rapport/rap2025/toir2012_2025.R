90ecac1bc8060187eeb5353d68ca942b2afc5b2e
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2026-01-06 17:40:46 +0100

    Add prevalence for cannabis use

--- rapport/rap2025/toir2012_2025.R
+++ rapport/rap2025/toir2012_2025.R
@@ -3,21 +3,65 @@
 pathToir <- file.path(here::here("rapport", "rap2025"))
 source(file.path(pathToir, "setup.R"))
 
-## ddt # data 2012-2024 er lastet i setup.R
-## DT  # data 2025 er lastet i setup.R
-
-DT[, year := 2025]
-
-commonCols <- intersect(names(ddt), names(DT))
-
-# clean labelled attributes coz it makes troubles
-dtx <- ddt[, lapply(.SD, \(col) {
-  attributes(col) <- NULL
-  col
-})]
+## Output from setup.R
+## ddt # data 2012-2024
+## DT  # data 2025
+## DTT # data 2012-2025
 
 
-DTT <- rbindlist(list(dtx[, ..commonCols], DT[, ..commonCols]), use.names = TRUE, fill = TRUE)
 ## ==================================
 ## Cannabis
 ## ----------------------------------
+
+DTT[can1 == 1, ltp_cannabis := 1] # Lifetime prevalence
+DTT[can6 == 1, lyp_cannabis := 1] # Last year prevalence
+DTT[can10 == 1, lmp_cannabis := 1] # Last month prevalence
+
+canLtpAllHist <- calc_percentage_ci(dt = DTT,
+                                outcome_var = "ltp_cannabis",
+                                weight_var = "vekt",
+                                denominator_var = "canpop",
+                                group_vars = "year",
+                                na_treatment = "as_zero",
+                                round_digits = 1,
+                                include_diagnostics = TRUE)
+
+canLypAllHist <- calc_percentage_ci(dt = DTT,
+                                outcome_var = "lyp_cannabis",
+                                weight_var = "vekt",
+                                denominator_var = "canpop",
+                                group_vars = "year",
+                                na_treatment = "as_zero",
+                                round_digits = 1,
+                                include_diagnostics = TRUE)
+
+canLmpAllHist <- calc_percentage_ci(dt = DTT,
+                                outcome_var = "lmp_cannabis",
+                                weight_var = "vekt",
+                                denominator_var = "canpop",
+                                group_vars = "year",
+                                na_treatment = "as_zero",
+                                round_digits = 1,
+                                include_diagnostics = TRUE)
+
+canLtpAllHist[, grp := "Noen gang"]
+canLypAllHist[, grp := "Siste år"]
+canLmpAllHist[, grp := "Siste måned"]
+
+canPrev <- rbindlist(list(canLtpAllHist, canLypAllHist, canLmpAllHist), use.names = TRUE, fill = TRUE)
+
+## create_ci_graph(data = canLtpAllHist,
+##                 x_col = "year",
+##                 y_col = "percentage",
+##                 lower_col = "ci_lower",
+##                 upper_col = "ci_upper")
+
+make_hist(
+  d = canPrev,
+  x = "year",
+  y = "percentage",
+  n = "n_level",
+  group = "grp",
+  type = "line",
+  title = "Cannabis prevalence 2012-2025"
+)
--- rapport/rap2025/toir2025.R
+++ rapport/rap2025/toir2025.R
@@ -3,8 +3,50 @@
 pathToir <- file.path(here::here("rapport", "rap2025"))
 source(file.path(pathToir, "setup.R"))
 
-DT  # data 2025 er lastet i setup.R
+# DT - data 2025 er lastet i setup.R
 
 ## ==================================
-## Lifetime prevalence
+## Cannabis
 ## ----------------------------------
+DT[can1 == 1, ltp_cannabis := 1] # Lifetime prevalence
+DT[can6 == 1, lyp_cannabis := 1] # Last year prevalence
+DT[can10 == 1, lmp_cannabis := 1] # Last month prevalence
+
+canLtpAll <- calc_percentage_ci(dt = DT,
+                                outcome_var = "ltp_cannabis",
+                                weight_var = "vekt",
+                                denominator_var = "canpop",
+                                na_treatment = "as_zero",
+                                round_digits = 1,
+                                include_diagnostics = TRUE)
+
+canLtpGender <- calc_percentage_ci(dt = DT,
+                                   outcome_var = "ltp_cannabis",
+                                   weight_var = "vekt",
+                                   group_vars = "kjonn",
+                                   denominator_var = "canpop",
+                                   na_treatment = "as_zero",
+                                   round_digits = 1,
+                                   include_diagnostics = TRUE)
+
+canLtpAge <- calc_percentage_ci(dt = DT,
+                                outcome_var = "ltp_cannabis",
+                                weight_var = "vekt",
+                                group_vars = "agecat",
+                                denominator_var = "canpop",
+                                na_treatment = "as_zero",
+                                round_digits = 1,
+                                include_diagnostics = TRUE)
+
+
+canLtpBoth <- calc_percentage_ci(dt = DT,
+                                 outcome_var = "ltp_cannabis",
+                                 weight_var = "vekt",
+                                 group_vars = c("kjonn","agecat"),
+                                 denominator_var = "canpop",
+                                 na_treatment = "as_zero",
+                                 round_digits = 1,
+                                 include_diagnostics = TRUE)
+
+setorder(canLtpBoth, "kjonn", "agecat")
+canLtpBoth[]
