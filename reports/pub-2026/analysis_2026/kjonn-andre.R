9f78621f82681f95cb49659e676aa69944bc4234
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2026-02-05 12:31:36 +0100

    Add sentralstimulant for 16-30

--- reports/pub-2026/analysis_2026/kjonn-andre.R
+++ reports/pub-2026/analysis_2026/kjonn-andre.R
@@ -56,49 +56,6 @@ return(out[])
 }
 
 
-append_total_row <- function(DT,
-                             factor_col = "kjonnSTR",
-                             label = "Totalt",
-                             weight_col = "sum_weights",
-                             pct_col = "percentage",
-                             sum_cols = c("n_total", "n_level", "sum_weights"),
-                             keep_outcome_level = FALSE) {
-  stopifnot(is.data.table(DT))
-
-  # Ensure label exists in factor levels if factor
-  if (is.factor(DT[[factor_col]])) {
-    levels(DT[[factor_col]]) <- union(levels(DT[[factor_col]]), label)
-  }
-
-  # Compute sums
-  sums <- DT[, lapply(.SD, sum, na.rm = TRUE), .SDcols = sum_cols]
-
-  # Weighted mean for percentage (safe if weights are zero)
-  w_total <- DT[[weight_col]]
-  p_total <- DT[[pct_col]]
-  wmean_pct <- if (!is.null(w_total) && sum(w_total, na.rm = TRUE) > 0) {
-    sum(p_total * w_total, na.rm = TRUE) / sum(w_total, na.rm = TRUE)
-  } else {
-    NA_real_
-  }
-
-  # Build total row with correct column order
-  tr <- as.list(rep(NA, ncol(DT)))
-  names(tr) <- names(DT)
-
-  tr[[factor_col]] <- if (is.factor(DT[[factor_col]])) factor(label, levels = levels(DT[[factor_col]])) else label
-  if (!keep_outcome_level && "outcome_level" %in% names(DT)) tr[["outcome_level"]] <- NA_real_
-  tr[[pct_col]] <- wmean_pct
-  for (nm in sum_cols) tr[[nm]] <- sums[[nm]]
-
-  # Clear CI by default
-  for (nm in c("ci_lower","ci_upper")) if (nm %in% names(DT)) tr[[nm]] <- NA_real_
-
-  total_row <- data.table::as.data.table(tr)
-  data.table::rbindlist(list(DT, total_row), use.names = TRUE, fill = TRUE)
-}
-
-
 kokainDT <- gender_prop(DTT, "cocaine", "kokain")
 mdmaDT <- gender_prop(DTT, "mdma", "mdma")
 ampheDT <- gender_prop(DTT, "amphetamines", "amfetaminer")
@@ -107,68 +64,10 @@ andreDT <- data.table::rbindlist(list(kokainDT, mdmaDT, ampheDT))
 andreDT[, percentage := round(percentage, 1)]
 
 ### ---- Yngre <=30
+### ------------------
 kokainDTyng <- gender_prop(DTT, "cocaine", "kokain", agemax = 30)
+mdmaDTyng <- gender_prop(DTT, "mdma", "mdma", agemax = 30)
+ampheDTyng <- gender_prop(DTT, "amphetamines", "amfetaminer", agemax = 30)
 
-
-
-
-
-## yr <- 2025
-
-## kokainLtp <- calc_percentage_total_ci(
-##   DTT[year == yr],
-##   outcome_var = "ltp_cocaine",
-##   group_vars = "kjonnSTR",
-##   weight_var = "vekt",
-##   denominator_var = "ltpPop_kokain",
-##   na_treatment = "as_zero",
-##   round_digits = 1,
-##   include_diagnostics = TRUE
-## )
-
-## kokainLyp <- calc_percentage_total_ci(
-##   DTT[year == yr],
-##   outcome_var = "lyp_cocaine",
-##   group_vars = "kjonnSTR",
-##   weight_var = "vekt",
-##   denominator_var = "lypPop_kokain",
-##   na_treatment = "as_zero",
-##   round_digits = 1,
-##   include_diagnostics = TRUE
-## )
-
-## kokainDT <- rbindlist(list(
-##   kokainLtp[, periode := "Noen gang"],
-##   kokainLyp[, periode := "Siste 12 måneder"]
-## ), use.names = TRUE, fill = TRUE)
-
-## ## Yngre aldersgrupper
-## ## -------------------------
-## ageMax <- 30
-
-## kokainLtpYng <- calc_percentage_total_ci(
-##   DTT[year == yr & alder <= ageMax],
-##   outcome_var = "ltp_cocaine",
-##   group_vars = "kjonnSTR",
-##   weight_var = "vekt",
-##   denominator_var = "ltpPop_kokain",
-##   na_treatment = "as_zero",
-##   round_digits = 1,
-##   include_diagnostics = TRUE
-## )
-
-## kokainLypYng <- calc_percentage_total_ci(
-##   DTT[year == yr & alder <= ageMax],
-##   outcome_var = "lyp_cocaine",
-##   group_vars = "kjonnSTR",
-##   weight_var = "vekt",
-##   denominator_var = "lypPop_kokain",
-##   na_treatment = "as_zero",
-##   round_digits = 1,
-##   include_diagnostics = TRUE
-## )
-
-## kokainDTYng <- rbindlist(list(
-##   kokainLtpYng[, periode := "Noen gang"],
-##   kokainLypYng[, periode := "Siste 12 måneder"]
-## ), use.names = TRUE, fill = TRUE)
+andreDTyng <- data.table::rbindlist(list(kokainDTyng, mdmaDTyng, ampheDTyng))
+andreDTyng[, percentage := round(percentage, 1)]
